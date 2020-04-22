385eb9bea38938fc45fb6fb0e01a952c73188974
blaqkrow <joshnfsmw@hotmail.com>
2020-04-23 00:18:36 +0800

    Initial commit

---
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker/common_widgets/custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton({
+    @required String assetName,
+    @required String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : 
+  assert(assetName != null),
+  assert(text != null),
+  super( //class which invokes super class constructor 
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Image.asset(assetName),
+        Text(
+          text,
+          style: TextStyle(color: textColor, fontSize: 15.0),
+        ),
+        Opacity(
+          child: Image.asset(assetName),
+          opacity: 0,
+        ),
+      ],
+    ),
+    color: color,
+    height: 50.0,
+    onPressed: onPressed,
+  );
+}
