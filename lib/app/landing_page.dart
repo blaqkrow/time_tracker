c87e0c5506f0002f4857587d4a2877e759166d5b
Joshua <blaqperformance@gmail.com>
2020-06-08 16:06:54 +0800

    firebase CRUD

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:time_tracker/app/home/jobs_page.dart';
 import 'package:time_tracker/services/auth.dart';
 import 'package:time_tracker/services/database.dart';
+import 'home/jobs/jobs_page.dart';
 import 'sign_in/sign_in_page.dart';
 
 class LandingPage extends StatelessWidget {
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,37 +1,30 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
+import 'dart:async';
+
+import 'package:meta/meta.dart';
 import 'package:time_tracker/app/home/models/job.dart';
 import 'package:time_tracker/services/api_path.dart';
+import 'package:time_tracker/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
-   Stream<List<Job>> jobsStream();
+  Stream<List<Job>> jobsStream();
 }
+
+String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
 
 class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
 
-  Future<void> createJob(Job job) async => await _setData(
-    path: APIPath.job(uid, 'job_abc'),
+  final _service = FirestoreService.instance;
+
+  Future<void> createJob(Job job) async => await _service.setData(
+    path: APIPath.job(uid, documentIdFromCurrentDate()),
     data: job.toMap(),
   );
 
-  Stream<List<Job>> jobsStream() {
-    final path = APIPath.jobs(uid);
-    final reference = Firestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.documents.map(
-      (snapshot) => Job(
-        name: snapshot.data['name'],
-        ratePerHour: snapshot.data['ratePerHour'],
-      ),
-    ).toList());
-  }
-
-  Future<void> _setData({String path, Map<String, dynamic> data}) async {
-    final reference = Firestore.instance.document(path);
-    print('$path: $data');
-    await reference.setData(data);
-  }
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
+    path: APIPath.jobs(uid),
+    builder: (data) => Job.fromMap(data),
+  );
 }
